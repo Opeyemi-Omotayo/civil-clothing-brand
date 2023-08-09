a77b75d27dabab87894ba974c95b029949e38ead
Opeyemi-Omotayo <Opeyemi.Motayo1@gmail.com>
2023-08-09 20:28:18 +0100

    adjusting

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,10 +9,12 @@ const Header = () => {
     <div className="relative w-full bg-cover bg-my-image h-95vh font-Lato">
       <NavBar />
       <div className="absolute transform -translate-x-1/2 -translate-y-1/2 top-80 left-1/2">
+        
         <h1 className="text-lg font-medium text-normal">Quiet Luxury</h1>
         <Button className="flex items-center justify-center w-64 p-3 my-5 rounded-md animate-bounce">
           Shop Now <BsArrowDown className="ml-3 text-xl" />
         </Button>
+
       </div>
     </div>
   );
